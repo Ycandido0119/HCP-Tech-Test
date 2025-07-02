57509c469d9186081c2c88cccb4eb6fb6239e318
Yassir Candido <yassircandido2@gmail.com>
2025-07-02 11:16:17 +0100

    Implement a Book and Sales model.

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 from csv import DictReader
+import datetime
+# Book model
+class Book:
+    def __init__(self, ISBN, title, binding, author, category, unit_price):
+        self.ISBN = int(ISBN)
+        self.title = title
+        self.binding = binding
+        self.author = author
+        self.category = category
+        self.unit_price = float(unit_price)
 
+    def __repr__(self):
+        return (
+            f"Book(ISBN={self.ISBN}, title='{self.title}', binding='{self.binding}', "
+            f"author='{self.author}', category='{self.category}', unit_price={self.unit_price})"
+        )
+
+# Sale model
+class Sale:
+    def __init__(self, date, ISBN, seller, quantity):
+        self.date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        self.ISBN = int(ISBN)
+        self.seller = seller
+        self.quantity = int(quantity)
+
+    def __repr__(self):
+        return (
+            f"Sale(date={self.date}, ISBN={self.ISBN}, seller='{self.seller}', "
+            f"quantity={self.quantity})"
+        )
 
 def load_books():
     with open("books.csv") as f:
